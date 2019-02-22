5cc932df65680e855087706991750e3aa812e79c
Dmitry Hryshuk <hryshuk@tut.by>
2019-02-22 14:37:34 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function warmup(temperature) {
   // your implementation here
+
+  if (isNaN(temperature)) {
+    return null;
+  }
+  let fahrenheit = temperature * 9 / 5 + 32;
+
+  return fahrenheit;
 };
